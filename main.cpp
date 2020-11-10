829ce2dfffab6d0481d2eac240b64a1e08c418af
Globson <samuelpedro1999@gmail.com>
2020-11-10 12:36:41 -0300

    Atualizei descrição

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "./Headers/Slotted_Aloha.hpp"
-using namespace std;
-int main(int argc, char const *argv[]) {
-  srand(time(NULL));
-  SlottedAloha();
-  cout<<endl;
-  return 0;
-}
