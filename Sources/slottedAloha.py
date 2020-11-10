ad65c5f0a7f448f0a2c7f2a7c0cddbb09701615f
Globson <samuelpedro1999@gmail.com>
2020-11-10 11:19:58 -0300

    Modifiquei alguns detalhes

--- Sources/slottedAloha.py
+++ Sources/slottedAloha.py
@@ -1,22 +1,20 @@
 from random import randint
 def SlottedAloha():
-    k=1
     print("O tamanho do frame é de 1 segundo\n")
-    n = int(input("Entre com a quantiade de estacoes:"))
+    n = int(input("Entre com a quantidade de estacoes:"))
     Tempo = [0 for i in range(n)]
     Pronto = [0 for i in range(n)]
     VetorAux = [0 for i in range(n)]
     Enviado = [0 for i in range(n)]
 
     for i in range(n):
-        Tempo[i]= randint(1,10)
+        Tempo[i]= randint(1,60)
         print("A estação ",i+1," ira enviar no tempo:  ",Tempo[i])
     Gatilho=0
     TempoAtual=1
     Aux=0
     Index=0
     Colisao=0
-    k=1
     while(Gatilho==0):
         for i in range(n):
             if(TempoAtual==Tempo[i] and Enviado[i]==0):
@@ -34,13 +32,12 @@ def SlottedAloha():
         if(Colisao==1):
             print("\nColisao detectada nas estacoes:\n")
             for i in range(Index):
-                Tempo[VetorAux[i]]=Tempo[VetorAux[i]]+k
+                Tempo[VetorAux[i]]=Tempo[VetorAux[i]]+randint(1,30)
                 print("\t( ",VetorAux[i]+1," ",end=')')
-                k=k*2
             if(Index>1):
-                print("\nO novo tempo de envio de cada estacao sera: ")
+                print("\n\nO novo tempo de envio de cada estacao sera: ")
             for i in range(Index):
-                print("Estacao: ",VetorAux[i]+1," -> ",Tempo[VetorAux[i]])
+                print("\tEstacao: ",VetorAux[i]+1," -> ",Tempo[VetorAux[i]])
         if(Colisao!=1 and Aux==1):
             Enviado[VetorAux[Index-1]]=1
             print("A estacao ",VetorAux[Index-1]+1, " enviou com sucesso! Tempo: ", TempoAtual)       
